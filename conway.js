dbd62d037849b6f39ae34efa4e3f9f53799c0f24
Marek Święcicki <marekswiecicki@protonmail.com>
2018-11-16 15:57:24 +0100

    Test and implement extinction of cells

--- conway.js
+++ conway.js
@@ -1,8 +1,46 @@
+const buildCoordsMap = input =>
+  input.reduce((coordsMap, coords) => ({
+    ...coordsMap,
+    [coords]: 0,
+  }), {});
 
-function conway() {
+const parseCoords = coords =>
+  coords.split(',').map(c => Number(c));
 
+const getNeighbours = ([x, y]) => [
+  `${x-1},${y-1}`, `${x},${y-1}`, `${x+1},${y-1}`,
+  `${x-1},${y}`,                  `${x+1},${y}`,
+  `${x-1},${y+1}`, `${x},${y+1}`, `${x+1},${y+1}`
+];
+
+const mergeArrays = (arr1, arr2) => arr1.concat(arr2);
+
+function conway(input) {
+  const coordsMap = input
+    .map(parseCoords)
+    .map(getNeighbours)
+    .reduce(mergeArrays, [])
+    .reduce((coordsMap, coords) => {
+      if (coordsMap[coords] !== undefined) {
+        coordsMap[coords] += 1;
+      }
+      return coordsMap;
+    }, buildCoordsMap(input));
+
+  return Object
+    .entries(coordsMap)
+    .reduce((survivors, [coords, neighbours]) => {
+      if (neighbours === 2 || neighbours === 3) {
+        return [...survivors, coords];
+      }
+      return survivors;
+    }, []);
 }
 
 module.exports = {
+  buildCoordsMap,
   conway,
+  getNeighbours,
+  mergeArrays,
+  parseCoords,
 };
--- conway.test.js
+++ conway.test.js
@@ -1,5 +1,98 @@
-const { conway } = require('./conway');
+const {
+  buildCoordsMap,
+  conway,
+  getNeighbours,
+  mergeArrays,
+  parseCoords
+} = require('./conway');
 
 test("Conway's game exists", () => {
   expect(conway).toBeDefined();
+});
+
+test("Conway's game takes an array and returns another array", () => {
+  const input = [];
+
+  const result = conway(input);
+
+  expect(result).toBeInstanceOf(Array);
+  expect(result).not.toBe(input);
+});
+
+test("Single cell dies in one generation", () => {
+  const input = ["1,1"];
+
+  const result = conway(input);
+
+  expect(result).toEqual([]);
+});
+
+test("Any cell without two or three neighbours dies in one generation", () => {
+  const input = [
+    "1,1", "2,1",
+    "1,2",
+    "1,3"
+  ];
+  const expectedResult = [
+    "1,1", "2,1",
+    "1,2",
+  ];
+
+  const result = conway(input);
+
+  expect(result).toEqual(expectedResult);
+});
+
+test("buildCoordsMap correctly transforms input to map", () => {
+  const input = ["11,13", "101,205", "53,678"];
+  const expectedResult = {
+    "11,13": 0,
+    "101,205": 0,
+    "53,678": 0,
+  };
+
+  const result = buildCoordsMap(input);
+
+  expect(result).toEqual(expectedResult);
+});
+
+test("buildCoordsMap returns empty map for empty input", () => {
+  const input = [];
+  const expectedResult = {};
+
+  const result = buildCoordsMap(input);
+
+  expect(result).toEqual(expectedResult);
+});
+
+test("parseCoords takes string of coords and splits it to array of numbers", () => {
+  const input = "12,-5";
+  const expectedResult = [12, -5];
+
+  const result = parseCoords(input);
+
+  expect(result).toEqual(expectedResult);
+});
+
+test("getNeighbours returns coords of neighbours of given cell", () => {
+  const input = [1,1];
+  const expectedResult = [
+    "0,0", "1,0", "2,0",
+    "0,1",        "2,1",
+    "0,2", "1,2", "2,2"
+  ];
+
+  const result = getNeighbours(input);
+
+  expect(result).toEqual(expectedResult);
+});
+
+test("mergeArrays joins two arrays", () => {
+  const arr1 = ["a", "b"];
+  const arr2 = [3,4];
+  const expectedResult = ["a", "b", 3, 4];
+
+  const result = mergeArrays(arr1, arr2);
+
+  expect(result).toEqual(expectedResult);
 });
